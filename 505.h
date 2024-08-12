29956f5cf7f955f75e84abf6bf0bc401bed36377
Noob404 <noob404yt@gmail.com>
2024-08-12 16:20:10 +0530

    feat: Added offsets for 5.05, 6.72, 7.02, 7.55 & 9.00

---
+++ 505.h
@@ -0,0 +1,10 @@
+#pragma once
+#define kern_off_xfast_syscall       0x1c0
+#define kern_off_allproc             0x02382FF8
+#define kern_off_vmspace_acquire_ref 0x0019EF90
+#define kern_off_vmspace_free        0x0019EDC0
+#define kern_off_printf              0x00436040
+#define kern_off_kmem_alloc          0x000FCC80
+#define kern_off_kernel_map          0x01AC60E0
+#define kern_off_sysent              0x0107C610
+#define kern_off_proc_rwmem          0x0030D150
---
+++ 755.h
@@ -0,0 +1,10 @@
+#pragma once
+#define kern_off_xfast_syscall       0x1c0
+#define kern_off_allproc             0x213c828
+#define kern_off_vmspace_acquire_ref 0x2fc290
+#define kern_off_vmspace_free        0x2fc0c0
+#define kern_off_printf              0x26f740
+#define kern_off_kmem_alloc          0x1753e0
+#define kern_off_kernel_map          0x21405b8
+#define kern_off_sysent              0x1122340
+#define kern_off_proc_rwmem          0x361310
